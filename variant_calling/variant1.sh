98602a292c744249bd9d0310a68e921556128bb4
Andrea Passetti <andrea.passetti91@gmail.com>
2024-09-13 17:47:54 +0200

    modified variant1.sh

--- variant_calling/variant1.sh
+++ variant_calling/variant1.sh
@@ -3,7 +3,7 @@
 ###################################################### DOWNLOAD STEP ####################################################################
 
 # make directories
-folder="/Users/andreapassetti/src/CFTR"
+folder="/home/mamba/src/CFTR"
 
 mkdir ${folder}
 mkdir ${folder}/Aligned ${folder}/Data ${folder}/Reads ${folder}/Results 
@@ -11,4 +11,3 @@ mkdir ${folder}/Aligned ${folder}/Data ${folder}/Reads ${folder}/Results
 prefetch SRR2136533
 fasterq-dump -x SRR2136533 -O ${folder}/Reads 
 #-t /Users/andreapassetti/tmp/scratch
-
