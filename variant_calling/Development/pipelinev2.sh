19b7bd3f12e13a134eb0d2fe68d215306545f343
Andrea Passetti <andrea.passetti91@gmail.com>
2024-09-16 14:10:19 +0200

    added checkpoints to pipelinev2

--- variant_calling/Development/pipelinev2.sh
+++ variant_calling/Development/pipelinev2.sh
@@ -3,7 +3,7 @@
 # Script to call germline variants in a human WGS paired end reads 2 X 100bp
 # Following GATK4 best practices workflow - https://gatk.broadinstitute.org/hc/en-us/articles/360035535932-Germline-short-variant-discovery-SNPs-Indels-
 
-###################################################### DOWNLOAD STEP ####################################################################
+###################################################### INITIAL STEP ####################################################################
 
 # Get the current home folder dynamically
 home_dir=$HOME
@@ -25,6 +25,26 @@ error_exit() {
 
 trap 'error_exit $LINENO' ERR
 
+# Define checkpoint file
+CHECKPOINT_FILE="${folder}checkpoint.txt"
+
+# Function to set the checkpoint
+set_checkpoint() {
+    echo "$1" > "$CHECKPOINT_FILE"
+}
+
+# Function to get the current checkpoint
+get_checkpoint() {
+    if [ -f "$CHECKPOINT_FILE" ]; then
+        cat "$CHECKPOINT_FILE"
+    else
+        echo "0" # Default to 0 if no checkpoint is found
+    fi
+}
+
+# Get the last checkpoint
+checkpoint=$(get_checkpoint)
+
 # Download data
 prefetch SRR2136533
 fasterq-dump -x SRR2136533 -O ${folder}/Reads
@@ -44,123 +64,149 @@ quality="${folder}/Quality"
 # STEP 1: QC - Run fastqc 
 # -------------------
 
-echo "STEP 1: QC - Run fastqc"
-
-parallel --eta fastqc {} -o ${quality}/ ::: ${reads}/SRR2136533_1.fastq ${reads}/SRR2136533_2.fastq
+if [ "$checkpoint" -le 1 ]; then
+    echo "STEP 1: QC - Run fastqc"
+    parallel --eta fastqc {} -o ${quality}/ ::: ${reads}/SRR2136533_1.fastq ${reads}/SRR2136533_2.fastq
+    
+    set_checkpoint 2 
+fi
 
 
 # --------------------------------------
 # STEP 2: Map to reference using BWA-MEM
 # --------------------------------------
 
-echo "STEP 2: Map to reference using BWA-MEM"
+if [ "$checkpoint" -le 2 ]; then
+    echo "STEP 2: Map to reference using BWA-MEM"
+    # BWA index reference 
+    bwa index ${ref}
+    samtools faidx ${ref}
 
-# BWA index reference 
-bwa index ${ref}
-samtools faidx ${ref}
-
-# BWA alignment with 8 threads
-bwa mem -t 8 -R "@RG\tID:SRR2136533\tPL:ILLUMINA\tSM:SRR2136533" ${ref} ${reads}/SRR2136533_1.fastq ${reads}/SRR2136533_2.fastq > ${aligned_reads}/SRR2136533.paired.sam
+    # BWA alignment with 8 threads
+    bwa mem -t 8 -R "@RG\tID:SRR2136533\tPL:ILLUMINA\tSM:SRR2136533" ${ref} ${reads}/SRR2136533_1.fastq ${reads}/SRR2136533_2.fastq > ${aligned_reads}/SRR2136533.paired.sam
+    
+    set_checkpoint 3 
+fi
 
 
 # -----------------------------------------
 # STEP 3: Mark Duplicates and Sort - GATK4
 # -----------------------------------------
 
-echo "STEP 3: Mark Duplicates and Sort - GATK4"
+if [ "$checkpoint" -le 3 ]; then
+    echo "STEP 3: Mark Duplicates and Sort - GATK4"
+    gatk MarkDuplicatesSpark -I ${aligned_reads}/SRR2136533.paired.sam -O ${aligned_reads}/SRR2136533_sorted_dedup_reads.bam
+    
+    set_checkpoint4 
+fi
 
-gatk MarkDuplicatesSpark -I ${aligned_reads}/SRR2136533.paired.sam -O ${aligned_reads}/SRR2136533_sorted_dedup_reads.bam
 
 # ----------------------------------
 # STEP 4: Base quality recalibration
 # ----------------------------------
 
-echo "STEP 4: Base quality recalibration"
+if [ "$checkpoint" -le 4 ]; then
+    echo "STEP 4: Base quality recalibration"
 
-# 1. Build the model
-gatk BaseRecalibrator -I ${aligned_reads}/SRR2136533_sorted_dedup_reads.bam -R ${ref} --known-sites ${known_sites} -O ${data}/recal_data.table
+    # 1. Build the model
+    gatk BaseRecalibrator -I ${aligned_reads}/SRR2136533_sorted_dedup_reads.bam -R ${ref} --known-sites ${known_sites} -O ${data}/recal_data.table
 
-# 2. Apply the model to adjust the base quality scores
-gatk ApplyBQSR -I ${aligned_reads}/SRR2136533_sorted_dedup_reads.bam -R ${ref} --bqsr-recal-file ${data}/recal_data.table -O ${aligned_reads}/SRR2136533_sorted_dedup_bqsr_reads.bam 
+    # 2. Apply the model to adjust the base quality scores
+    gatk ApplyBQSR -I ${aligned_reads}/SRR2136533_sorted_dedup_reads.bam -R ${ref} --bqsr-recal-file ${data}/recal_data.table -O ${aligned_reads}/SRR2136533_sorted_dedup_bqsr_reads.bam 
+    
+    set_checkpoint5
+fi
 
 
 # -----------------------------------------------
 # STEP 5: Collect Alignment & Insert Size Metrics
 # -----------------------------------------------
 
-echo "STEP 5: Collect Alignment & Insert Size Metrics"
+if [ "$checkpoint" -le 5 ]; then
+    echo "STEP 5: Collect Alignment & Insert Size Metrics"
 
-gatk CollectAlignmentSummaryMetrics -R ${ref} -I ${aligned_reads}/SRR2136533_sorted_dedup_bqsr_reads.bam -O ${aligned_reads}/alignment_metrics.txt
-gatk CollectInsertSizeMetrics INPUT=${aligned_reads}/SRR2136533_sorted_dedup_bqsr_reads.bam OUTPUT=${aligned_reads}/insert_size_metrics.txt HISTOGRAM_FILE=${aligned_reads}/insert_size_histogram.pdf
+    gatk CollectAlignmentSummaryMetrics -R ${ref} -I ${aligned_reads}/SRR2136533_sorted_dedup_bqsr_reads.bam -O ${aligned_reads}/alignment_metrics.txt
+    gatk CollectInsertSizeMetrics INPUT=${aligned_reads}/SRR2136533_sorted_dedup_bqsr_reads.bam OUTPUT=${aligned_reads}/insert_size_metrics.txt HISTOGRAM_FILE=${aligned_reads}/insert_size_histogram.pdf
+    set_checkpoint6
+fi
 
 
 # ----------------------------------------------
 # STEP 6: Call Variants - gatk haplotype caller (parallelized)
 # ----------------------------------------------
 
-echo "STEP 6: Call Variants - gatk haplotype caller"
+if [ "$checkpoint" -le 6 ]; then
+    echo "STEP 6: Call Variants - gatk haplotype caller"
 
-# Parallel variant calling using multiple regions (genome is large, splitting it speeds things up)
-gatk HaplotypeCaller \
-    -R ${ref} \
-    -I ${aligned_reads}/SRR2136533_sorted_dedup_bqsr_reads.bam \
-    -O ${results}/raw_variants.vcf \
-    -ERC GVCF \
-    -L chr1 -L chr2 -L chr3 -L chr4 -L chr5 -L chr6 -L chr7 -L chr8 -L chr9 -L chr10 -L chr11 \
-    --native-pair-hmm-threads 8
+    # Parallel variant calling using multiple regions (genome is large, splitting it speeds things up)
+    gatk HaplotypeCaller \
+        -R ${ref} \
+        -I ${aligned_reads}/SRR2136533_sorted_dedup_bqsr_reads.bam \
+        -O ${results}/raw_variants.vcf \
+        -ERC GVCF \
+        -L chr1 -L chr2 -L chr3 -L chr4 -L chr5 -L chr6 -L chr7 -L chr8 -L chr9 -L chr10 -L chr11 \
+        --native-pair-hmm-threads 8
 
 
-# extract SNPs & INDELS
-gatk SelectVariants -R ${ref} -V ${results}/raw_variants.vcf --select-type SNP -O ${results}/raw_snps.vcf
-gatk SelectVariants -R ${ref} -V ${results}/raw_variants.vcf --select-type INDEL -O ${results}/raw_indels.vcf
+    # extract SNPs & INDELS
+    gatk SelectVariants -R ${ref} -V ${results}/raw_variants.vcf --select-type SNP -O ${results}/raw_snps.vcf
+    gatk SelectVariants -R ${ref} -V ${results}/raw_variants.vcf --select-type INDEL -O ${results}/raw_indels.vcf
+    set_checkpoint7
+fi
 
 
 # -------------------
 # STEP 7: Filter Variants - GATK4
 # -------------------
 
-# Filter SNPs
-gatk VariantFiltration \
-    -R ${ref} \
-    -V ${results}/raw_snps.vcf \
-    -O ${results}/filtered_snps.vcf \
-    -filter-name "QD_filter" -filter "QD < 2.0" \
-    -filter-name "FS_filter" -filter "FS > 60.0" \
-    -filter-name "MQ_filter" -filter "MQ < 40.0" \
-    -filter-name "SOR_filter" -filter "SOR > 4.0" \
-    -filter-name "MQRankSum_filter" -filter "MQRankSum < -12.5" \
-    -filter-name "ReadPosRankSum_filter" -filter "ReadPosRankSum < -8.0" \
-    -genotype-filter-expression "DP < 10" \
-    -genotype-filter-name "DP_filter" \
-    -genotype-filter-expression "GQ < 10" \
-    -genotype-filter-name "GQ_filter"
+if [ "$checkpoint" -le 7 ]; then
+    echo "STEP 7: Filtering Variants"
+    # Filter SNPs
+    gatk VariantFiltration \
+        -R ${ref} \
+        -V ${results}/raw_snps.vcf \
+        -O ${results}/filtered_snps.vcf \
+        -filter-name "QD_filter" -filter "QD < 2.0" \
+        -filter-name "FS_filter" -filter "FS > 60.0" \
+        -filter-name "MQ_filter" -filter "MQ < 40.0" \
+        -filter-name "SOR_filter" -filter "SOR > 4.0" \
+        -filter-name "MQRankSum_filter" -filter "MQRankSum < -12.5" \
+        -filter-name "ReadPosRankSum_filter" -filter "ReadPosRankSum < -8.0" \
+        -genotype-filter-expression "DP < 10" \
+        -genotype-filter-name "DP_filter" \
+        -genotype-filter-expression "GQ < 10" \
+        -genotype-filter-name "GQ_filter"
 
-# Filter INDELS
-gatk VariantFiltration \
-    -R ${ref} \
-    -V ${results}/raw_indels.vcf \
-    -O ${results}/filtered_indels.vcf \
-    -filter-name "QD_filter" -filter "QD < 2.0" \
-    -filter-name "FS_filter" -filter "FS > 200.0" \
-    -filter-name "SOR_filter" -filter "SOR > 10.0" \
-    -genotype-filter-expression "DP < 10" \
-    -genotype-filter-name "DP_filter" \
-    -genotype-filter-expression "GQ < 10" \
-    -genotype-filter-name "GQ_filter"
+    # Filter INDELS
+    gatk VariantFiltration \
+        -R ${ref} \
+        -V ${results}/raw_indels.vcf \
+        -O ${results}/filtered_indels.vcf \
+        -filter-name "QD_filter" -filter "QD < 2.0" \
+        -filter-name "FS_filter" -filter "FS > 200.0" \
+        -filter-name "SOR_filter" -filter "SOR > 10.0" \
+        -genotype-filter-expression "DP < 10" \
+        -genotype-filter-name "DP_filter" \
+        -genotype-filter-expression "GQ < 10" \
+        -genotype-filter-name "GQ_filter"
 
 
-# Select Variants that PASS filters
-gatk SelectVariants --exclude-filtered -V ${results}/filtered_snps.vcf -O ${results}/analysis-ready-snps.vcf
-gatk SelectVariants --exclude-filtered -V ${results}/filtered_indels.vcf -O ${results}/analysis-ready-indels.vcf
+    # Select Variants that PASS filters
+    gatk SelectVariants --exclude-filtered -V ${results}/filtered_snps.vcf -O ${results}/analysis-ready-snps.vcf
+    gatk SelectVariants --exclude-filtered -V ${results}/filtered_indels.vcf -O ${results}/analysis-ready-indels.vcf
 
-# to exclude variants that failed genotype filters
-grep -v -E "DP_filter|GQ_filter" ${results}/analysis-ready-snps.vcf > ${results}/analysis-ready-snps-filteredGT.vcf
-grep -v -E "DP_filter|GQ_filter" ${results}/analysis-ready-indels.vcf > ${results}/analysis-ready-indels-filteredGT.vcf
+    # to exclude variants that failed genotype filters
+    grep -v -E "DP_filter|GQ_filter" ${results}/analysis-ready-snps.vcf > ${results}/analysis-ready-snps-filteredGT.vcf
+    grep -v -E "DP_filter|GQ_filter" ${results}/analysis-ready-indels.vcf > ${results}/analysis-ready-indels-filteredGT.vcf
 
-# Annotate VCF with functional information
-snpEff -v GRCh38.99 ${results}/analysis-ready-snps-filteredGT.vcf > ${results}/analysis-ready-snps-annotated.vcf
-snpEff -v GRCh38.99 ${results}/analysis-ready-indels-filteredGT.vcf > ${results}/analysis-ready-indels-annotated.vcf
+    # Annotate VCF with functional information
+    snpEff -v GRCh38.99 ${results}/analysis-ready-snps-filteredGT.vcf > ${results}/analysis-ready-snps-annotated.vcf
+    snpEff -v GRCh38.99 ${results}/analysis-ready-indels-filteredGT.vcf > ${results}/analysis-ready-indels-annotated.vcf
 
-# Filter annotated variants
-SnpSift filter "(ANN[*].IMPACT = 'HIGH') | (ANN[*].IMPACT = 'MODERATE')" ${results}/analysis-ready-snps-annotated.vcf > ${results}/analysis-ready-snps-highImpact.vcf
-SnpSift filter "(ANN[*].IMPACT = 'HIGH') | (ANN[*].IMPACT = 'MODERATE')" ${results}/analysis-ready-indels-annotated.vcf > ${results}/analysis-ready-indels-highImpact.vcf
+    # Filter annotated variants
+    SnpSift filter "(ANN[*].IMPACT = 'HIGH') | (ANN[*].IMPACT = 'MODERATE')" ${results}/analysis-ready-snps-annotated.vcf > ${results}/analysis-ready-snps-highImpact.vcf
+    SnpSift filter "(ANN[*].IMPACT = 'HIGH') | (ANN[*].IMPACT = 'MODERATE')" ${results}/analysis-ready-indels-annotated.vcf > ${results}/analysis-ready-indels-highImpact.vcf
+
+    set_checkpoint 8 # Final checkpoint (script completed)
+fi
+echo "Script completed."
