0e8a5d176aa45cde2c1395215a83ec0c0f49d55e
Andrea Passetti <andrea.passetti91@gmail.com>
2024-10-04 14:22:59 +0200

    added option to save the search

--- genesearch/genesearch.py
+++ genesearch/genesearch.py
@@ -8,6 +8,7 @@ import shlex
 # Define reference and input files
 home_dir = os.path.expanduser("~")
 ref_file = os.path.join(home_dir, "Reference", "hg38", "gencode.v46.annotation.gff3.gz")
+save_fle = os.path.join(home_dir, "Documents")
 
 def main():
     # Check if reference file exists
@@ -21,15 +22,23 @@ def main():
         sys.exit("Gene name cannot be empty")
 
     # Execute bash command
-    command = f"gunzip -c {shlex.quote(ref_file)} | grep -v '#' | awk '$3 == \"gene\" && $9 ~ \"gene_name={shlex.quote(gene_name)}\"' | less -S -x 15"
+    command = f"gunzip -c {shlex.quote(ref_file)} | grep -v '#' | awk '$3 == \"gene\" && $9 ~ \"gene_name={gene_name}\"' | tee {shlex.quote(save_fle)}/{shlex.quote(gene_name)}.txt | less -S -x 15"
     try:
         subprocess.run(command, shell=True, check=True)
+        save_output = input("Do you want to save the gene information to a file? (y/n): ").strip().lower()
+        
+        if save_output == "n":
+            os.remove(f"{save_fle}/{gene_name}.txt")
+            sys.exit("Gene information not saved")
+        else:            
+        print(f"Gene information saved to {save_fle}/{gene_name}.txt")
+    
     except subprocess.CalledProcessError as e:
         sys.exit(f"An error occurred while executing the command: {e}")
 
     # Ask if user wants to search again
-    search_again = input("Do you want to search for another gene? (yes/no): ").strip().lower()
-    if search_again == "yes":
+    search_again = input("Do you want to search for another gene? (y/n): ").strip().lower()
+    if search_again == "y":
         main()
     else:
         sys.exit("Goodbye!")
